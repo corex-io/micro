0e0ab0f047e90ea312f4a86a7cbaed63d26c03bb
luopengift <870148195@qq.com>
2023-02-17 16:58:49 +0800

    update

--- limit/every.go
+++ limit/every.go
@@ -5,72 +5,51 @@ import (
 	"errors"
 	"time"
 
-	"github.com/corex-io/micro/log"
+	"golang.org/x/sync/errgroup"
 )
 
 var StopErr = errors.New("stop limit.Every")
 
 // Every every
 type Every struct {
-	duration time.Duration
-	f        func(ctx context.Context) error
-	stop     chan error
+	wait time.Duration
+	f    func(ctx context.Context) error
+	st   func(ctx context.Context) error
 }
 
 // NewEvery newEvery
-func NewEvery(duration time.Duration, f func(ctx context.Context) error) *Every {
+func NewEvery(wait time.Duration, f func(ctx context.Context) error) *Every {
 	return &Every{
-		duration: duration,
-		f:        f,
-		stop:     make(chan error),
+		wait: wait,
+		f:    f,
 	}
 }
 
-// Stop stop
-func (v *Every) Stop(err error) {
-	v.stop <- err
-}
-
-// Run 间隔every秒开始下一次
-func (v *Every) Run(ctx context.Context) error {
-	t := time.NewTimer(0 * time.Second)
-	defer t.Stop()
-	for {
-		select {
-		case <-t.C:
-			if err := v.f(ctx); err != nil {
-				if errors.Is(err, StopErr) {
-					return nil
-				}
-				log.Warnf("every: %s", err)
-			}
-			t.Reset(v.duration)
-		case <-ctx.Done():
-			return ctx.Err()
-		case err := <-v.stop:
-			return err
-		}
-	}
+func (v *Every) Sentry(f func(ctx context.Context) error) {
+	v.st = f
 }
 
 // Run 每隔every秒开始执行下一次，如果一次任务的运行时长超过了every，那么下一次直接发起
-func (v *Every) Sched(ctx context.Context) error {
+func (v *Every) Sched(ctx context.Context, exit func(error) bool) error {
+	group, ctx := errgroup.WithContext(ctx)
+	defer group.Wait()
+	if v.st != nil {
+		group.Go(func() error {
+			return v.st(ctx)
+		})
+	}
+
 	t := time.NewTimer(0 * time.Second)
 	defer t.Stop()
 	for {
 		select {
 		case <-t.C:
-			t.Reset(v.duration)
-			if err := v.f(ctx); err != nil {
-				if errors.Is(err, StopErr) {
-					return err
-				}
-				log.Errorf("%s", err)
+			if err := v.f(ctx); exit(err) {
+				return err
 			}
+			t.Reset(v.wait)
 		case <-ctx.Done():
 			return ctx.Err()
-		case err := <-v.stop:
-			return err
 		}
 	}
 }
